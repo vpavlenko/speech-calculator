818a19b66be00a96f855e5e80aa794333f856e70
Vitaly Pavlenko <cxielamiko@gmail.com>
2015-03-25 22:33:12 +0300

    Added while true loop with recursive AsyncTasks

--- app/src/main/java/ru/skoltech/language/MainActivity.java
+++ app/src/main/java/ru/skoltech/language/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
@@ -52,155 +53,149 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				byte[] PCMSound = writeSpeechChunkToPCM();
-                String text = recognizePCMFile(PCMSound);
-                Log.i("JSON RESPONSE", text);
-                //{"result":[{"alternative":[{"transcript":"друг","confide
-                //{"result":[{"alternative":[{"transcript":"мой дядя","confidence":0.93204647}],"final":true}],"result_index":0}
-                String transcript;
-                try {
-                    transcript = new JSONObject(text.split("\\r?\\n")[1]).getJSONArray("result")
-                            .getJSONObject(0).getJSONArray("alternative").getJSONObject(0)
-                            .getString("transcript");
-                    Log.i("TRANSCRIPT", transcript);
-                    txtQuery.setText(txtQuery.getText() + "\n" + transcript);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+
 			}
 		});
 
+        new RecognizeChunkTask().execute();
 	}
 
-    private String recognizePCMFile(byte[] PCMSound) {
-        HttpRequest request = HttpRequest.post("https://www.google.com/speech-api/v2/recognize",
-                true, "output", "json", "lang", "ru-ru", "key", "AIzaSyDbzyMzbi_vNpEvjhO_ob8mEVMxi70FUuI")
-                .header("Content-Type", "audio/l16; rate=16000;")
-                .trustAllCerts()
-                .trustAllHosts()
-                .send(PCMSound);
+    class RecognizeChunkTask extends AsyncTask<Void, Void, Void> {
+        private String transcript;
 
-        return request.body();
-    }
+        @Override
+        protected Void doInBackground(Void... params) {
+            byte[] PCMSound = writeSpeechChunkToPCM();
 
-	/**
-	 * Showing google speech input dialog
-	 * */
-	private void promptSpeechInput() {
-		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-				RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en_US");
-		intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-				getString(R.string.speech_prompt));
-		try {
-			startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-		} catch (ActivityNotFoundException a) {
-			Toast.makeText(getApplicationContext(),
-					getString(R.string.speech_not_supported),
-					Toast.LENGTH_SHORT).show();
-		}
-	}
+            new RecognizeChunkTask().execute();
 
-    private byte[] writeSpeechChunkToPCM() {
-        int RECORDER_SAMPLERATE = 16000;
-        int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
-        int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
-
-        int bufferSizeInBytes = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE,
-                RECORDER_CHANNELS,
-                RECORDER_AUDIO_ENCODING
-        );
-
-        // Initialize Audio Recorder.
-        AudioRecord audioRecorder = new AudioRecord( MediaRecorder.AudioSource.MIC,
-                RECORDER_SAMPLERATE,
-                RECORDER_CHANNELS,
-                RECORDER_AUDIO_ENCODING,
-                bufferSizeInBytes
-        );
-        // Start Recording.
-        audioRecorder.startRecording();
-
-        int numberOfReadBytes   = 0;
-        byte audioBuffer[]      = new  byte[bufferSizeInBytes];
-        boolean recording       = false;
-        float tempFloatBuffer[] = new float[3];
-        int tempIndex           = 0;
-        int totalReadBytes      = 0;
-        byte totalByteBuffer[]  = new byte[60 * RECORDER_SAMPLERATE * 1];
-
-
-        // While data come from microphone.
-        while( true )
-        {
-            float totalAbsValue = 0.0f;
-            short sample        = 0;
-
-            numberOfReadBytes = audioRecorder.read( audioBuffer, 0, bufferSizeInBytes );
-
-            // Analyze Sound.
-            for( int i=0; i<bufferSizeInBytes; i+=2 )
-            {
-                sample = (short)( (audioBuffer[i]) | audioBuffer[i + 1] << 8 );
-                totalAbsValue += Math.abs( sample ) / (numberOfReadBytes/2);
+            String text = recognizePCMFile(PCMSound);
+            Log.i("JSON RESPONSE", text);
+            
+            try {
+                transcript = new JSONObject(text.split("\\r?\\n")[1]).getJSONArray("result")
+                        .getJSONObject(0).getJSONArray("alternative").getJSONObject(0)
+                        .getString("transcript");
+                Log.i("TRANSCRIPT", transcript);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            // Analyze temp buffer.
-            tempFloatBuffer[tempIndex%3] = totalAbsValue;
-            float temp                   = 0.0f;
-            for( int i=0; i<3; ++i )
-                temp += tempFloatBuffer[i];
-
-            if( (temp >=0 && temp <= 350) && recording == false )
-            {
-//                Log.i("TAG", "1");
-                tempIndex++;
-                continue;
-            }
-
-            if( temp > 350 && recording == false )
-            {
-//                Log.i("TAG", "2");
-                recording = true;
-            }
-
-            if( (temp >= 0 && temp <= 350) && recording == true )
-            {
-                Log.i("TAG", "Save audio to file.");
-
-                // Save audio to file.
-                String filepath = Environment.getExternalStorageDirectory().getPath();
-                File file = new File(filepath,"AudioRecorder");
-                if( !file.exists() )
-                    file.mkdirs();
-
-                String fileName = file.getAbsolutePath() + "/" + System.currentTimeMillis() + ".pcm";
-                Log.i("FILENAME", fileName);
-
-                int realSize = totalByteBuffer.length;
-                while (totalByteBuffer[realSize - 1] == 0) {
-                    realSize--;
-                }
-
-                audioRecorder.stop();
-                return Arrays.copyOfRange(totalByteBuffer, 0, realSize);
-            }
-
-            // -> Recording sound here.
-            Log.i( "TAG", "Recording Sound." );
-            for( int i=0; i<numberOfReadBytes; i++ )
-                totalByteBuffer[totalReadBytes + i] = audioBuffer[i];
-            totalReadBytes += numberOfReadBytes;
-            //*/
-
-            tempIndex++;
+            return null;
         }
 
-    }
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (transcript != null) {
+                txtQuery.setText(txtQuery.getText() + "\n" + transcript);
+            }
+        }
 
-    private void promptSpeechInputNew() {
-//        SpeechRecognizer sr = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
+        private String recognizePCMFile(byte[] PCMSound) {
+            HttpRequest request = HttpRequest.post("https://www.google.com/speech-api/v2/recognize",
+                    true, "output", "json", "lang", "ru-ru", "key", "AIzaSyDbzyMzbi_vNpEvjhO_ob8mEVMxi70FUuI")
+                    .header("Content-Type", "audio/l16; rate=16000;")
+                    .trustAllCerts()
+                    .trustAllHosts()
+                    .send(PCMSound);
+
+            return request.body();
+        }
+
+        private byte[] writeSpeechChunkToPCM() {
+            int RECORDER_SAMPLERATE = 16000;
+            int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+            int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+
+            int bufferSizeInBytes = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE,
+                    RECORDER_CHANNELS,
+                    RECORDER_AUDIO_ENCODING
+            );
+
+            // Initialize Audio Recorder.
+            AudioRecord audioRecorder = new AudioRecord( MediaRecorder.AudioSource.MIC,
+                    RECORDER_SAMPLERATE,
+                    RECORDER_CHANNELS,
+                    RECORDER_AUDIO_ENCODING,
+                    bufferSizeInBytes
+            );
+            // Start Recording.
+            audioRecorder.startRecording();
+
+            int numberOfReadBytes   = 0;
+            byte audioBuffer[]      = new  byte[bufferSizeInBytes];
+            boolean recording       = false;
+            float tempFloatBuffer[] = new float[3];
+            int tempIndex           = 0;
+            int totalReadBytes      = 0;
+            byte totalByteBuffer[]  = new byte[60 * RECORDER_SAMPLERATE * 1];
+
+
+            // While data come from microphone.
+            while( true )
+            {
+                float totalAbsValue = 0.0f;
+                short sample        = 0;
+
+                numberOfReadBytes = audioRecorder.read( audioBuffer, 0, bufferSizeInBytes );
+
+                // Analyze Sound.
+                for( int i=0; i<bufferSizeInBytes; i+=2 )
+                {
+                    sample = (short)( (audioBuffer[i]) | audioBuffer[i + 1] << 8 );
+                    totalAbsValue += Math.abs( sample ) / (numberOfReadBytes/2);
+                }
+
+                // Analyze temp buffer.
+                tempFloatBuffer[tempIndex%3] = totalAbsValue;
+                float temp                   = 0.0f;
+                for( int i=0; i<3; ++i )
+                    temp += tempFloatBuffer[i];
+
+                if( (temp >=0 && temp <= 350) && recording == false )
+                {
+                    tempIndex++;
+                    continue;
+                }
+
+                if( temp > 350 && recording == false )
+                {
+                    recording = true;
+                }
+
+                if( (temp >= 0 && temp <= 350) && recording == true )
+                {
+                    Log.i("TAG", "Save audio to file.");
+
+                    // Save audio to file.
+                    String filepath = Environment.getExternalStorageDirectory().getPath();
+                    File file = new File(filepath,"AudioRecorder");
+                    if( !file.exists() )
+                        file.mkdirs();
+
+                    String fileName = file.getAbsolutePath() + "/" + System.currentTimeMillis() + ".pcm";
+                    Log.i("FILENAME", fileName);
+
+                    int realSize = totalByteBuffer.length;
+                    while (totalByteBuffer[realSize - 1] == 0) {
+                        realSize--;
+                    }
+
+                    audioRecorder.stop();
+                    return Arrays.copyOfRange(totalByteBuffer, 0, realSize);
+                }
+
+                // -> Recording sound here.
+                Log.i( "TAG", "Recording Sound." );
+                for( int i=0; i<numberOfReadBytes; i++ )
+                    totalByteBuffer[totalReadBytes + i] = audioBuffer[i];
+                totalReadBytes += numberOfReadBytes;
+                //*/
+
+                tempIndex++;
+            }
+        }
+
     }
 
 	/**
