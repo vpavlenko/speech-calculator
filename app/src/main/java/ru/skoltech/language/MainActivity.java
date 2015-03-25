42c0e41308f8ae6e2f207e93f64b97736e2d32b9
Vitaly Pavlenko <cxielamiko@gmail.com>
2015-03-25 21:17:22 +0300

    Recording chunks to PCM signed 16-bit 16000 Hz

--- app/src/main/java/ru/skoltech/language/MainActivity.java
+++ app/src/main/java/ru/skoltech/language/MainActivity.java
@@ -9,21 +9,17 @@ import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
 import android.speech.RecognizerIntent;
-import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Locale;
 
 
 public class MainActivity extends Activity {
@@ -70,8 +66,7 @@ public class MainActivity extends Activity {
 	}
 
     private void promptSpeechInputContinuous() {
-        int RECORDER_SAMPLERATE = 44100;
-        byte RECORDER_BPP = 16;
+        int RECORDER_SAMPLERATE = 16000;
         int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
         int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
 
@@ -143,70 +138,18 @@ public class MainActivity extends Activity {
                 if( !file.exists() )
                     file.mkdirs();
 
-                String fn = file.getAbsolutePath() + "/" + System.currentTimeMillis() + ".wav";
+                String fn = file.getAbsolutePath() + "/" + System.currentTimeMillis() + ".pcm";
                 Log.i("FILENAME", fn);
 
-                long totalAudioLen  = 0;
-                long totalDataLen   = totalAudioLen + 36;
-                long longSampleRate = RECORDER_SAMPLERATE;
-                int channels        = 1;
-                long byteRate       = RECORDER_BPP * RECORDER_SAMPLERATE * channels/8;
-                totalAudioLen       = totalReadBytes;
-                totalDataLen        = totalAudioLen + 36;
-                byte finalBuffer[]  = new byte[totalReadBytes + 44];
-
-                finalBuffer[0] = 'R';  // RIFF/WAVE header
-                finalBuffer[1] = 'I';
-                finalBuffer[2] = 'F';
-                finalBuffer[3] = 'F';
-                finalBuffer[4] = (byte) (totalDataLen & 0xff);
-                finalBuffer[5] = (byte) ((totalDataLen >> 8) & 0xff);
-                finalBuffer[6] = (byte) ((totalDataLen >> 16) & 0xff);
-                finalBuffer[7] = (byte) ((totalDataLen >> 24) & 0xff);
-                finalBuffer[8] = 'W';
-                finalBuffer[9] = 'A';
-                finalBuffer[10] = 'V';
-                finalBuffer[11] = 'E';
-                finalBuffer[12] = 'f';  // 'fmt ' chunk
-                finalBuffer[13] = 'm';
-                finalBuffer[14] = 't';
-                finalBuffer[15] = ' ';
-                finalBuffer[16] = 16;  // 4 bytes: size of 'fmt ' chunk
-                finalBuffer[17] = 0;
-                finalBuffer[18] = 0;
-                finalBuffer[19] = 0;
-                finalBuffer[20] = 1;  // format = 1
-                finalBuffer[21] = 0;
-                finalBuffer[22] = (byte) channels;
-                finalBuffer[23] = 0;
-                finalBuffer[24] = (byte) (longSampleRate & 0xff);
-                finalBuffer[25] = (byte) ((longSampleRate >> 8) & 0xff);
-                finalBuffer[26] = (byte) ((longSampleRate >> 16) & 0xff);
-                finalBuffer[27] = (byte) ((longSampleRate >> 24) & 0xff);
-                finalBuffer[28] = (byte) (byteRate & 0xff);
-                finalBuffer[29] = (byte) ((byteRate >> 8) & 0xff);
-                finalBuffer[30] = (byte) ((byteRate >> 16) & 0xff);
-                finalBuffer[31] = (byte) ((byteRate >> 24) & 0xff);
-                finalBuffer[32] = (byte) (2 * 16 / 8);  // block align
-                finalBuffer[33] = 0;
-                finalBuffer[34] = RECORDER_BPP;  // bits per sample
-                finalBuffer[35] = 0;
-                finalBuffer[36] = 'd';
-                finalBuffer[37] = 'a';
-                finalBuffer[38] = 't';
-                finalBuffer[39] = 'a';
-                finalBuffer[40] = (byte) (totalAudioLen & 0xff);
-                finalBuffer[41] = (byte) ((totalAudioLen >> 8) & 0xff);
-                finalBuffer[42] = (byte) ((totalAudioLen >> 16) & 0xff);
-                finalBuffer[43] = (byte) ((totalAudioLen >> 24) & 0xff);
-
-                for( int i=0; i<totalReadBytes; ++i )
-                    finalBuffer[44+i] = totalByteBuffer[i];
+                int realSize = totalByteBuffer.length;
+                while (totalByteBuffer[realSize - 1] == 0) {
+                    realSize--;
+                }
 
                 FileOutputStream out;
                 try {
                     out = new FileOutputStream(fn);
-                    out.write(finalBuffer);
+                    out.write(totalByteBuffer, 0, realSize);
                     out.close();
                 } catch (IOException e) {
                     e.printStackTrace();
